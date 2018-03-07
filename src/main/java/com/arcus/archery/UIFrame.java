3ed18195450fa44684771eceb4f8775e052f7939
xalf <Aliaksandr_Fedasiuk@epam.com>
2018-03-08 02:14:12 +0300

    code review 1.0.7

--- src/main/java/com/arcus/archery/UIFrame.java
+++ src/main/java/com/arcus/archery/UIFrame.java
@@ -16,6 +16,7 @@ import java.util.TimerTask;
 public class UIFrame extends JFrame {
 
     public static final String STOP_SHOOTING = "<html><div style=\"text-align:center\">STOP<br>SHOOTING</div></html>";
+    public static final String STOP = "STOP";
     DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 
     static final String ARIAL_BLACK = "Arial Black";
@@ -136,18 +137,20 @@ public class UIFrame extends JFrame {
 
     class PreShotCountdownTimer extends TimerTask {
         int cnt = 10;
+
+        public PreShotCountdownTimer() {
+            setRedSchema();
+        }
+
         @Override
         public void run() {
+            //System.out.println("PreShotCountdownTimer : " + dateFormat.format(Calendar.getInstance().getTime())
+            // + " :" + cnt + ", round: " + globalCount + ", isStopShooting = " + isStopShooting);
             if (isStopShooting || isSkipRound) {
-                System.out.println("PreShotCountdownTimer stop : " + dateFormat.format(Calendar.getInstance().getTime())
-                        + " :" + cnt + ", round: " + globalCount);
                 cancel();
                 init();
             } else {
-                System.out.println("PreShotCountdownTimer: " + dateFormat.format(Calendar.getInstance().getTime())
-                        + " :" + cnt + ", round: " + globalCount);
                 count.setFont(new Font(ARIAL_BLACK, Font.BOLD, 500));
-                setRedSchema();
                 count.setText(String.valueOf(cnt));
                 cnt--;
                 if (cnt <= 0) {
@@ -171,42 +174,50 @@ public class UIFrame extends JFrame {
     class ShotTimer extends TimerTask {
         int cnt = 20;
 
+        public ShotTimer() {
+            setGreenSchema();
+            count.setFont(new Font(ARIAL_BLACK, Font.BOLD, 500));
+            count.setText(String.valueOf(cnt));
+            team.setText(TARGET + ((selectedTeam ? 1 : 0) + 1));
+        }
+
         @Override
         public void run() {
+            //System.out.println("ShootTimer: " + dateFormat.format(Calendar.getInstance().getTime())
+            // + " :" + cnt + ", round: " + globalCount + ", isStopShooting = " + isStopShooting);
             if (isStopShooting) {
-                System.out.println("ShotTimer stop : " + dateFormat.format(Calendar.getInstance().getTime()) + " :" + cnt + ", round: " + globalCount);
                 cancel();
                 init();
             } else {
-                System.out.println("ShotTimer: " + dateFormat.format(Calendar.getInstance().getTime()) + " :" + cnt + ", round: " + globalCount);
-                if (cnt > 3) {
-                    setGreenSchema();
-                } else {
+                cnt--;
+                if (cnt < 4 || isSkipRound) {
                     setRedSchema();
                 }
-                count.setText(String.valueOf(cnt));
-                team.setText(TARGET + ((selectedTeam ? 1 : 0) + 1));
-                cnt--;
+
+                if (cnt <= 0 || isSkipRound) {
+                    count.setFont(new Font(ARIAL_BLACK, Font.BOLD, 300));
+                    count.setText(STOP);
+                } else {
+                    count.setText(String.valueOf(cnt));
+                }
+
                 if (cnt < 1 || isSkipRound) {
-                    if (isSkipRound) {
-                        count.setText(String.valueOf("0"));
-                        setRedSchema();
-                    }
                     isSkipRound = false;
                     globalCount--;
                     if (globalCount > 0) {
+                        selectedTeam = !selectedTeam;
                         try {
                             Thread.sleep(1000);
                         } catch (InterruptedException e) {
                             e.printStackTrace();
                         }
-                        selectedTeam = !selectedTeam;
                         startShortTimer();
                     } else {
                         count.setFont(new Font(ARIAL_BLACK, Font.BOLD, 200));
+                        count.setVisible(false);
                         count.setText(STOP_SHOOTING);
+                        count.setVisible(true);
                         team.setText("");
-                        setRedSchema();
                         timer.scheduleAtFixedRate(new Beeper(3), 1000, 1000);
                         isActive = false;
                         isStopShooting = true;
@@ -231,9 +242,11 @@ public class UIFrame extends JFrame {
 
     class Beeper extends TimerTask {
         int cnt;
+
         public Beeper(int cnt) {
             this.cnt = cnt;
         }
+
         @Override
         public void run() {
             beep();
@@ -247,7 +260,7 @@ public class UIFrame extends JFrame {
 
     private void beep() {
         try {
-            System.out.println("Beep");
+            //System.out.println("Beep");
             SoundUtils.tone(600, 500);
         } catch (LineUnavailableException e) {
             e.printStackTrace();
